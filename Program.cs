e9cb821d3b997e36b38fc229acd70cb72038f203
Animesh <Animesh.Das@gyansys.com>
2022-11-25 11:05:17 +0530

    Single Ton Design

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SingleTonDesign {
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            SingleTonDesign singleTondesign = new SingleTonDesign.Instance;
+            singleTondesign.SingleTonMethod();
+        }
+    }
+}
+
+
---
+++ SingleTonDesign.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonDesign
+{
+    public class SingleTonDesign
+    {
+
+        private static SingleTonDesign instance = new SingleTonDesign();
+        private SingleTonDesign()
+        {
+
+        }
+
+        public static SingleTonDesign Instance
+        {
+            get { return instance; }
+        }
+
+        public void SingleTonMethod()
+        {
+            Console.WriteLine("Singleton class code has been called");
+        }
+
+    }
+}
